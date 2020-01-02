cb87d361357237acb22914bd6dcd408fe51386a6
Martin Lenz <martinlenz@Martins-MacBook-Pro.local>
2020-01-02 15:11:07 +0100

    Create test-sensor.py

---
+++ test-sensor.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import serial
+import time
+import sys
+
+import Adafruit_DHT
+
+sensor = '22'
+pin = '4'
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+print(humidity)
+print(temperature)
