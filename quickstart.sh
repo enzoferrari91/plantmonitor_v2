5b733b225ba7c62213e05d3ec0cb5ef1d8895db7
Martin Lenz <martinlenz@Martins-MacBook-Pro.local>
2020-01-05 23:58:41 +0100

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -2,6 +2,9 @@ echo "WELCOME TO PLANTMONITOR V2!"
 echo "QUICKSTART"
 sudo apt-get update
 sudo apt-get install git -y
+sudo rm -rv plantmonitor_v2
+sudo rm -rv plantmonitor_v2-logs
+sudo rm -rv plantmonitor_v2-db
 sudo git clone https://github.com/enzoferrari91/plantmonitor_v2.git
 cd plantmonitor_v2
 clear
